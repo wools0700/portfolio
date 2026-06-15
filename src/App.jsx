6e608f79c068832a9a841211026174005f4ecd78
wools0700 <https://github.com/wools0700>
2026-06-15 19:20:15 +0900

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Project1 from "./pages/Project1";
-import Project2 from "./pages/Project2";
-import Project3 from "./pages/Project3";
 
 import "./css/home.css";
 import "./css/detail.css";
@@ -13,9 +10,6 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/project1" element={<Project1 />} />
-                <Route path="/project2" element={<Project2 />} />
-                <Route path="/project3" element={<Project3 />} />
             </Routes>
         </BrowserRouter>
     );
