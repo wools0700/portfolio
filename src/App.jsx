f35387ee8ba296499ebbad6f8f4cda0cddec0344
wools0700 <https://github.com/wools0700>
2026-06-16 22:15:59 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-
 import Project1 from "./pages/Project1";
 import Project2 from "./pages/Project2";
 
@@ -10,13 +9,13 @@ import "./css/detail.css";
 
 function App() {
     return (
-        <BrowserRouter basename="/portfolio">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/project1" element={<Project1 />} />
                 <Route path="/project2" element={<Project2 />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
